26227cba63e027e38d0988a09724bd0559464595
Daryl Ronningen <relms@relms.dev>
2022-08-07 15:09:50 -0700

    copy to system_ext instead

--- priv-app/Lawnchair/Android.mk
+++ priv-app/Lawnchair/Android.mk
@@ -2,11 +2,13 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := Lawnchair
-LOCAL_MODULE_STEM := Lawnchair.apk
 LOCAL_SRC_FILES := Lawnchair.apk
 LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH := $(TARGET_OUT)/priv-app/Lawnchair
-LOCAL_OVERRIDES_PACKAGES := ParanoidQuickStep Launcher3QuickStep PixelLauncher TrebuchetQuickStep NexusLauncherRelease
+LOCAL_MODULE_CLASS := APPS
+LOCAL_OVERRIDES_PACKAGES := NexusLauncherRelease Launcher3 Launcher3QuickStep
+LOCAL_SYSTEM_EXT_MODULE := true
+LOCAL_PRIVILEGED_MODULE := true
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 
 include $(BUILD_PREBUILT)
